5a6f1884c56b2363fd230419eab6daf8344a3937
Anh Chu <anh.chu@Anhs-MacBook-Pro.local>
2025-06-11 21:12:03 +0200

    fix: update Tailwind CSS configuration and styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/features/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
