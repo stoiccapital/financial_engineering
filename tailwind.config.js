8a44f0bc9c0306d4bec2ae19fdd835e427515380
Anh Chu <anh.chu@Anhs-MacBook-Pro.local>
2025-06-12 11:29:12 +0200

    fix: update Tailwind configuration to scan all files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    // add any other directories where you use Tailwind classes
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
